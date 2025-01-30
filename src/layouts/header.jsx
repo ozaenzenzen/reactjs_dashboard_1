34afaf1e85503b69cdfba7cc8bce5d4251cadc5f
Fauzan Akmal Mahdi <recovery252@gmail.com>
2025-01-30 16:48:26 +0700

    add search bar

--- src/layouts/header.jsx
+++ src/layouts/header.jsx
@@ -1,10 +1,31 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { ChevronLast, ChevronLeft, ChevronsLeft, Search } from "lucide-react";
 
 const Header = ({ collapsed, setCollapsed }) => {
     return (
         <header className="relative z-10 flex h-[60px] items-center justify-between bg-white px-4 shadow-md transition-colors dark:bg-slate-900">
-            Header
+            <div className="flex items-center gap-x-3">
+                <button
+                    className="btn-ghost size-10"
+                    onClick={() => setCollapsed(!collapsed)}
+                >
+                    <ChevronsLeft className={collapsed && "rotate-180"} />
+                </button>
+                <div className="input">
+                    <Search
+                        size={20}
+                        className="text-slate-300"
+                    />
+                    <input
+                        type="text"
+                        name="search"
+                        id="search"
+                        placeholder="Search"
+                        className="w-full bg-transparent text-slate-900 outline-0 placeholder:text-slate-300 dark:text-slate-50"
+                    />
+                </div>
+            </div>
         </header>
     );
 };
