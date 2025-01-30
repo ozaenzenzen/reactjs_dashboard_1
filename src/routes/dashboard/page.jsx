c20290791a39cdd918b3c3d9f76e57d775f2977d
Fauzan Akmal Mahdi <recovery252@gmail.com>
2025-01-30 18:16:45 +0700

    add overview

--- src/routes/dashboard/page.jsx
+++ src/routes/dashboard/page.jsx
@@ -1,8 +1,14 @@
 import { CreditCard, DollarSign, Package, TrendingUp, Users } from "lucide-react";
 import React from "react";
 import CardSummaryComponent from "./component/CardSummaryComponent";
+import { Area, AreaChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
+
+import { overviewData } from "@/constants";
+import { useTheme } from "@/hooks/use-theme";
 
 const DashboardPage = () => {
+    const { theme } = useTheme();
+
     return (
         <div className="flex flex-col gap-y-4">
             <h1 className="title">DashboardPage</h1>
@@ -44,6 +50,77 @@ const DashboardPage = () => {
                     isTrendingUp="true"
                     trendingValue="55%"
                 />
+            </div>
+            <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-7">
+                <div className="card col-span-1 md:col-span-2 lg:col-span-4">
+                    <div className="card-header">
+                        <p className="card-title">Overview</p>
+                    </div>
+                    <div className="card-body p-0">
+                        <ResponsiveContainer
+                            width="100%"
+                            height={300}
+                        >
+                            <AreaChart
+                                data={overviewData}
+                                margin={{
+                                    top: 0,
+                                    right: 0,
+                                    left: 0,
+                                    bottom: 0,
+                                }}
+                            >
+                                <defs>
+                                    <linearGradient
+                                        id="colorTotal"
+                                        x1="0"
+                                        y1="0"
+                                        x2="0"
+                                        y2="1"
+                                    >
+                                        <stop
+                                            offset="5%"
+                                            stopColor="#2563eb"
+                                            stopOpacity={0.8}
+                                        />
+                                        <stop
+                                            offset="95%"
+                                            stopColor="#2563eb"
+                                            stopOpacity={0}
+                                        />
+                                    </linearGradient>
+                                </defs>
+                                <Tooltip
+                                    cursor={false}
+                                    formatter={(value) => `$${value}`}
+                                />
+
+                                <XAxis
+                                    dataKey="name"
+                                    strokeWidth={0}
+                                    stroke={theme === "light" ? "#475569" : "#94a3b8"}
+                                    tickMargin={6}
+                                />
+                                <YAxis
+                                    dataKey="total"
+                                    strokeWidth={0}
+                                    stroke={theme === "light" ? "#475569" : "#94a3b8"}
+                                    tickFormatter={(value) => `$${value}`}
+                                    tickMargin={6}
+                                />
+
+                                <Area
+                                    type="monotone"
+                                    dataKey="total"
+                                    stroke="#2563eb"
+                                    fillOpacity={1}
+                                    fill="url(#colorTotal)"
+                                />
+                            </AreaChart>
+                        </ResponsiveContainer>
+                    </div>
+                </div>
+                <div className="card col-span-1 md:col-span-2 lg:col-span-3"></div>
             </div>
         </div>
     );
