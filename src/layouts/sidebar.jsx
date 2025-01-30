e7a14f56e288dda33752df95418538f6c9b0042a
Fauzan Akmal Mahdi <recovery252@gmail.com>
2025-01-30 15:36:29 +0700

    setup sidebar

--- src/layouts/sidebar.jsx
+++ src/layouts/sidebar.jsx
@@ -1,11 +1,16 @@
 import { forwardRef } from "react";
 import { cn } from "../utils/cn";
 
+import { navbarLinks } from "../constants";
+
 import React from "react";
 import logoLight from "../assets/logo-light.svg";
 import logoDark from "../assets/logo-dark.svg";
 
-const Sidebar = forwardRef(({}, ref) => {
+import PropTypes from "prop-types";
+import { NavLink } from "react-router-dom";
+
+const Sidebar = forwardRef(({ collapsed }, ref) => {
     return (
         <aside
             ref={ref}
@@ -24,10 +29,37 @@ const Sidebar = forwardRef(({}, ref) => {
                     alt="Logoipsum2"
                     className="hidden dark:block"
                 />
-            </div> 
+                {!collapsed && <p className="text-lg font-medium text-slate-900 transition-colors dark:text-slate-50">Fauzan's Lab</p>}
+            </div>
+            <div className="flex w-full flex-col gap-y-4 overflow-y-auto overflow-x-hidden p-3 [scrollbar-width:_thin]">
+                {navbarLinks.map((navbarLink) => (
+                    <nav
+                        key={navbarLink.title}
+                        className={cn("sidebar-group")}
+                    >
+                        <p className={cn("sidebar-group-title")}>{navbarLink.title}</p>
+                        {navbarLink.links.map((link) => (
+                            <NavLink
+                                key={link.label}
+                                to={link.path}
+                                className={cn("sidebar-item")}
+                            >
+                                <link.icon
+                                    size={22}
+                                    className="shrink-8 flex"
+                                />
+                                {!collapsed && <p className="whitespace-nowrap">{link.label}</p>}
+                            </NavLink>
+                        ))}
+                    </nav>
+                ))}
+            </div>
         </aside>
     );
 });
 Sidebar.displayName = "Sidebar";
+Sidebar.propTypes = {
+    collapsed: PropTypes.bool,
+};
 
 export default Sidebar;
--- src/routes/layout.jsx
+++ src/routes/layout.jsx
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import Sidebar from "../layouts/sidebar";
+
+import { useMediaQuery } from "@uidotdev/usehooks";
 
 import { cn } from "../utils/cn";
 import Header from "../layouts/header";
 import { Outlet } from "react-router-dom";
 
 const Layout = () => {
+    const isDesktopDevice = useMediaQuery("(min-width: 768px)");
+    const [collapsed, setCollapsed] = useState(false);
+
+    const sidebarRef = useRef(null);
+
     return (
         <div className="min-h-screen bg-slate-100 transition-colors dark:bg-slate-950">
-            <Sidebar />
+            <Sidebar
+                ref={sidebarRef}
+                collapsed={collapsed}
+            />
             <div className={cn("transition-[margin] duration-300")}>
                 <Header />
                 <div className="h-[calc(100vh-60px)] overflow-y-auto overflow-x-hidden p-6">
